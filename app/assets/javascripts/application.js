773a748d3d6549503fbe7e2946ef9de0e1a643f8
Joshua Briley <josh@thebrileys.com>
2021-09-19 21:48:22 -0400

    Layout updates.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,14 @@
 // require rails-ujs
 // require turbolinks
 //= require_tree
+
+// console.log('I WAS LOADED')
+// const togglePaneButton = document.querySelectorAll('.toggle-side-pane')
+// console.log(togglePaneButton)
+// const sidePane = document.querySelector('.side-pane')
+// togglePaneButton.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     console.log('clicked')
+//     sidePane.removeClass('-translate-y-full')
+//   })
+// })
