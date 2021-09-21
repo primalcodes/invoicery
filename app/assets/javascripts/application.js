323ef7ef8cec4fa6d16726b1d687c93eb40ff684
Joshua Briley <josh@thebrileys.com>
2021-09-21 13:50:07 -0400

    Fix toggle pane issue.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,14 @@
 //     sidePane.removeClass('-translate-y-full')
 //   })
 // })
+
+const toggleContainer = () => {
+  const togglePaneButton = document.querySelectorAll('.toggle-side-pane')
+  const sidePane = document.querySelector('.side-pane')
+  const sidePaneContent = document.querySelector('.side-pane-content')
+
+  sidePane.classList.toggle('-translate-x-full')
+  sidePane.classList.toggle('translate-x-0')
+  sidePaneContent.classList.toggle('-translate-x-full')
+  sidePaneContent.classList.toggle('translate-x-0')
+}
