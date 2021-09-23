e854a8c7eb82a5bcd4ea6b35674829e6e0f315dd
Joshua Briley <josh@thebrileys.com>
2021-09-22 23:17:46 -0400

    Update line item totals.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,17 +2,6 @@
 // require turbolinks
 //= require_tree
 
-// console.log('I WAS LOADED')
-// const togglePaneButton = document.querySelectorAll('.toggle-side-pane')
-// console.log(togglePaneButton)
-// const sidePane = document.querySelector('.side-pane')
-// togglePaneButton.forEach((button) => {
-//   button.addEventListener('click', () => {
-//     console.log('clicked')
-//     sidePane.removeClass('-translate-y-full')
-//   })
-// })
-
 const toggleContainer = () => {
   const togglePaneButton = document.querySelectorAll('.toggle-side-pane')
   const sidePane = document.querySelector('.side-pane')
